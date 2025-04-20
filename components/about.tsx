50e24a7d2f119a95ecb9b3adbed6bbcf8732a8ed
guzamak <120812215+guzamak@users.noreply.github.com>
2025-04-20 18:52:23 +0700

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -9,7 +9,7 @@ export default function About() {
       className="pt-20 bg-gradient-to-b from-white to-gray-50 relative"
     >
       <div className="container mx-auto px-6">
-        <div className="flex justify-center items-center gap-4 mb-16">
+        <div className="flex justify-center items-center gap-3 mb-16">
           <Image
             src="/assert/eye.svg"
             alt="Hackathon Banner"
@@ -21,12 +21,27 @@ export default function About() {
             About The Event
           </h2>
         </div>
-
+        <div className="w-full h-fit p-5 text-center mb-3.5">
+          <p>
+            <p className="font-bold mb-3">Mini Hack by JWC13</p> เป็นกิจกรรม Hackathon จัดขึ้นโดยพี่ ๆ จากค่าย JWC
+            (Junior Webmater Camp) โดยจะส่งต่อประสบการณ์สุดพิเศษผ่านรูปแบบ
+            Hackathon ไป-กลับ 2 วันเต็ม สำหรับน้อง ๆ ม.ปลาย
+            ที่สนใจเทคโนโลยีและอยากเติบโตในวงการ Webmater
+            ไม่ว่าจะด้านการเขียนโปรแกรม การออกแบบ หรือการเสนอไอเดียใหม่ ๆ
+            ผ่านรูปแบบเว็บไซต์ น้องจะได้รวมทีมสร้างเว็บไซต์ ร่วมกิจกรรมสนุก ๆ
+            ทำความรู้จักเพื่อนใหม่ และได้พูดคุยพบปะกับพี่ ๆ จากค่าย JWC ตัวจริง
+            ซึ่ง Hackathon ในครั้งนี้พวกเรามาในธีม "One-Page Wonder
+            สร้างประสบการณ์เว็บทั้งระบบในหน้าเดียว" เปิดโอกาสให้น้อง ๆ
+            แสดงไอเดียความคิดสร้างสรรค์อย่างเต็มที่ในระยะเวลา 2 วัน "ที่นี่...
+            เราจะพิสูจน์ให้เห็นว่า
+            การเรียนรู้เทคโนโลยีสามารถสนุกและสร้างสรรค์ได้ทุกคน!"
+          </p>
+        </div>
         <div className="grid md:grid-cols-3 gap-10">
           <div className="p-8 border-2 border-gray-200 rounded-3xl hover:bg-gray-100 hover:scale-105  duration-200">
             <FileJson2 className="w-8 h-8 text-gray-300 my-2" />
             <h3 className="text-xl font-bold mb-3 font-Playfair-Display ">
-              48 Hours of Coding
+              48 Hours of Hackathon
             </h3>
             <p className="text-gray-600">
               การทำโปรเจกต์สุดโหดในเวลา 48 ชั่วโมง
@@ -37,9 +52,7 @@ export default function About() {
             <h3 className="text-xl font-bold mb-3 font-Playfair-Display ">
               1 Page Wonder Theme
             </h3>
-            <p className="text-gray-600">
-              หลายหน้าก็ไม่ได้ หลายใจก็ไม่ดี
-            </p>
+            <p className="text-gray-600">หลายหน้าก็ไม่ได้ หลายใจก็ไม่ดี</p>
           </div>
           <div className="p-8 border-2 border-gray-200 rounded-3xl hover:bg-gray-100 hover:scale-105 duration-200">
             <Award className="w-8 h-8 text-gray-300 my-2" />
@@ -65,7 +78,9 @@ export default function About() {
             />
 
             <div className="h-36 flex justify-center items-center gap-4">
-              <p className="text-2xl text-gray-700 font-semibold">&quot; ?????? &quot;</p>
+              <p className="text-2xl text-gray-700 font-semibold">
+                &quot; ?????? &quot;
+              </p>
             </div>
           </div>
         </div>
