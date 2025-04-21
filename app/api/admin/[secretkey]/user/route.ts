c9e1a745e871d8b625466e83ec9c8404fe7b487c
guzamak <120812215+guzamak@users.noreply.github.com>
2025-04-21 18:53:35 +0700

    Update route.ts

--- app/api/admin/[secretkey]/user/route.ts
+++ app/api/admin/[secretkey]/user/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import prisma from '@/lib/prisma'
 
-export async function GET(_: NextRequest, context: { params: { secretkey: string } }) {
+export async function GET(req: NextRequest, context: { params: { secretkey: string } }) {
   const { secretkey } = context.params;
   // console.log(params.secretkey)
   // secetkey = param 
